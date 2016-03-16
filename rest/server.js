921f3a15478c8a74be114504c009c1e130bcdc2b
chefArchitect <c.spiliotopoulos@internetq.com>
2016-03-16 15:25:20 +0200

    Upgraded to latest HapiJS version

--- rest/server.js
+++ rest/server.js
@@ -1,114 +1,14 @@
-/*
- * Dependencies
+var nconf = require('nconf');
+var path = require('path');
+
+var BootstrapService = require('./services/BootstrapService');
+
+
+/**
+ * Setup the service
  */
-var Hapi = require('hapi');
-var Good = require('good');
-var Joi = require('joi');
-var hapiSwaggered = require('hapi-swaggered');
-var hapiSwaggeredUi = require('hapi-swaggered-ui');
+BootstrapService.setup(function(){
 
-/*
- * Hapi server instance
- */
-var server = new Hapi.Server();
 
-server.connection({
-	port : 3000,
-	labels : [ 'api' ],
-	app: {
-    swagger: {
-      info: {
-        title: 'API Spots - BDD API Testing',
-        description: 'Test your APIs using BDD',
-        version: '0.0.1',
-        contact: {
-        	name: 'Chris Spiliotopoulos (@chefArchitect)',
-        	url: 'https://github.com/chefArchitect',
-        	email: 'chrysanthos.spiliotopoulos@gmail.com'
-        },
-        license: {
-        	name: 'MIT',
-        	url: 'http://opensource.org/licenses/MIT'
-        }
-      }
-    }
-  }
-});
 
-/*
- * Good console
- */
-server.register({
-	register : Good,
-	options : {
-		reporters : [ {
-			reporter : require('good-console'),
-			events : {
-				response : '*',
-				log : '*'
-			}
-		} ]
-	}
-}, function(err) {
-	if (err) {
-		throw err; // something bad happened loading the plugin
-	}
-	
-	server.start(function() {
-		server.log('info', 'Server running at: ' + server.info.uri);
-	});
-});
-
-/*
- * Swagger plugin options
- */
-server.register({
-	register : hapiSwaggered,
-	options : {
-		tags : {
-			'/foobar' : 'Example foobar description'
-		},
-		info : {
-			title : 'Example API',
-			description : 'Tiny hapi-swaggered example',
-			version : '1.0'
-		}
-	}
-}, {
-	select : 'api',
-	routes : {
-		prefix : '/swagger'
-	}
-}, function(err) {
-	if (err) {
-		throw err
-	}
-});
-
-server.register({
-	register : hapiSwaggeredUi,
-	options : {
-		title : 'API Spots - BDD API Testing'
-	}
-}, {
-	select : 'api',
-	routes : {
-		prefix : '/docs'
-	}
-}, function(err) {
-	if (err) {
-		throw err
-	}
-});
-
-/*
- * load plugins & API routes
- */
-require('./routes')(server);
-
-/*
- * start the server
- */
-server.start(function() {
-	server.log('Server running at:', server.info.uri);
 });
--- rest/services/BootstrapService.js
+++ rest/services/BootstrapService.js
@@ -1,119 +1,220 @@
 /**
  * Bootstrap service.
- * 
- * 
+ *
+ *
  */
 
 var log = require('winston');
 var fs = require('fs');
 var async = require("async");
 var nconf = require('nconf');
+var Hapi = require('hapi');
+var Path = require('path');
+var Good = require('good');
+var Joi = require('joi');
+var Inert = require("inert");
+var Vision = require("vision");
+var hapiSwaggered = require('hapi-swaggered');
+var hapiSwaggeredUi = require('hapi-swaggered-ui');
 
 module.exports = function() {
-	
-	// is the environment initialized?
-	var _initialized = false;
-	
-	// Array to hold async tasks
-	var _asyncTasks = [];
-	
-	/**
-	 * Sets up the logging context
-	 */
-	function _setupLogging(callback) {
-		
-		/*
-		 * setup logging framework
-		 */
-		log.info('Initializing logging framework...');
-		
-		// Console transport
-		if ((nconf.get()) && (typeof nconf.get().logging != 'undefined')
-				&& (typeof nconf.get().logging.console != 'undefined')
-				&& (typeof nconf.get().logging.console.level != 'undefined')) {
-			
-			// log.transports.Console.level = nconf.get().logging.console.level;
-			log.remove(log.transports.Console);
-			
-			log.add(log.transports.Console, {
-				level : nconf.get().logging.console.level,
-				colorize : true
-			});
-			
-			log.info('|_ Added Console transport [Level: '
-					+ nconf.get().logging.console.level + ']');
-		}
-		
-		// File transport support
-		if ((nconf.get()) && (typeof nconf.get().logging != 'undefined')
-				&& (typeof nconf.get().logging.file != 'undefined')
-				&& (typeof nconf.get().logging.file.filename != 'undefined')) {
-			
-			log.add(log.transports.File, {
-				level : nconf.get().logging.file.level,
-				filename : nconf.get().logging.file.filename,
-				maxsize : nconf.get().logging.file.maxSize,
-				maxFiles : nconf.get().logging.file.maxFiles,
-				inlineMeta : true
-			});
-			
-			log.info('|_ Added File transport [Log file: '
-					+ nconf.get().logging.file.filename + ']');
-		}
-		
-		// PaperTrail transport support
-		if ((nconf.get()) && (typeof nconf.get().logging != 'undefined')
-				&& (typeof nconf.get().logging.papertrail != 'undefined')) {
-			log.add(Papertrail, {
-				level : nconf.get().logging.papertrail.level,
-				host : nconf.get().logging.papertrail.host,
-				port : nconf.get().logging.papertrail.port,
-				logFormat : function(level, message) {
-					return '[' + level + '] ' + message;
-				},
-				inlineMeta : true
-			})
 
-			log.info('|_ Added PaperTrail transport [Host: '
-					+ nconf.get().logging.papertrail.host + ' Port: '
-					+ +nconf.get().logging.papertrail.port + ']');
-			
-		}
-		
-		return callback();
-	}
-	
-	return {
-		
-		/**
-		 * 
-		 */
-		setup : function(callback) {
-			
-			if (_initialized)
-				return;
-			
-			log.info('==========================================');
-			log.info('Setting up contexts...');
-			
-			// setup logging
-			_asyncTasks.push(_setupLogging);
-			
-			async.parallel(_asyncTasks, function() {
-				
-				log.info('All contexts configured successfuly.');
-				log.info('===========================================');
-				
-				_initialized = true;
-				
-				// notify listeners
-				if (callback)
-					return callback();
-				
-			});
-			
-		}
-	
-	};
-	
+  // is the environment initialized?
+  var _initialized = false;
+
+  // Array to hold async tasks
+  var _asyncTasks = [];
+
+  // the server instance
+  var _server = null;
+
+  /**
+   * Sets up the logging context
+   */
+  function _setupLogging(callback) {
+
+    /*
+     * setup logging framework
+     */
+    log.info('Initializing logging framework...');
+
+    // Console transport
+    if ((nconf.get()) && (typeof nconf.get().logging != 'undefined') && (typeof nconf.get().logging.console != 'undefined') && (typeof nconf.get().logging.console.level != 'undefined')) {
+
+      // log.transports.Console.level = nconf.get().logging.console.level;
+      log.remove(log.transports.Console);
+
+      log.add(log.transports.Console, {
+        level: nconf.get().logging.console.level,
+        colorize: true
+      });
+
+      log.info('|_ Added Console transport [Level: ' + nconf.get().logging.console.level + ']');
+    }
+
+    if (callback)
+      callback();
+  }
+
+  /**
+   * Sets up the server
+   */
+  function _setupServer() {
+
+    try {
+
+      /*
+       * Hapi server instance
+       */
+      _server = new Hapi.Server();
+
+      /*
+       * set the connection info
+       * and Swagger plugin
+       */
+      _server.connection({
+				port: 3000,
+        labels: ['api'],
+        routes: {
+          cors: false
+        }
+      });
+
+    } catch (e) {
+      console.error('Error setting up server: ' + e);
+    }
+  }
+
+  /**
+   * Sets up the server plugins
+   */
+  function _setupServerPlugins() {
+
+    try {
+
+      /*
+       * Swagger plugin options
+       */
+
+      var opts = {
+        schemes: ['http', 'https'],
+        cors: false,
+        info: {
+          title: 'API Spots - BDD API Testing',
+          description: 'Test your APIs using BDD',
+          version: '1.0.0',
+          contact: {
+            name: 'Chris Spiliotopoulos (@chefArchitect)',
+            url: 'https://github.com/chefArchitect',
+            email: 'chrysanthos.spiliotopoulos@gmail.com'
+          }
+        }
+      };
+
+      _server.register([
+        Inert,
+        Vision,
+        /* register other plugins including hapi-swaggered and hapi-swaggered-ui */
+      ], function(err) {})
+
+      _server.register({
+        register: hapiSwaggered,
+        options: opts
+      }, {
+        select: 'api',
+        routes: {
+
+        }
+      }, function(err) {
+        if (err) {
+          throw err
+        }
+      });
+
+      /*
+       * Hapi Swaggered UI plugin
+       */
+      opts = {};
+
+      _server.register({
+        register: hapiSwaggeredUi,
+        options: opts
+      }, {
+        select: 'api',
+        routes: {
+          prefix: '/docs'
+        }
+      }, function(err) {
+        if (err) {
+          throw err
+        }
+      });
+
+    } catch (e) {
+      console.error('Error setting up server plugins: ' + e);
+    }
+  }
+
+  /**
+   * Sets up the server plugins
+   */
+  function _setupRouting(callback) {
+
+    try {
+
+      /*
+       * load API routes
+       */
+      require('../routes')(_server);
+
+    } catch (e) {
+      console.error('Error setting up routes: ' + e);
+    }
+  }
+
+  return {
+
+    /**
+     *
+     */
+    setup: function(callback) {
+
+      if (_initialized)
+        return;
+
+      log.info('==========================================');
+      log.info('Setting up contexts...');
+
+      // setup logging
+      _setupLogging();
+
+      // setup server
+      _setupServer();
+
+      // setup the plugins
+      _setupServerPlugins();
+
+      // setup routing
+      _setupRouting();
+
+      /*
+       * start the server
+       */
+      _server.start(function() {
+
+        log.info('Server running at:', _server.info.uri);
+
+        log.info('==========================================');
+
+        _initialized = true;
+
+        // notify listeners
+        if (callback)
+          callback();
+      });
+
+    }
+
+  };
+
 }();
